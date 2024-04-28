fa56aaf067a81cb2801c8580c5df28e3b3e5871c
Sumukh Prasad <sumukhprasad@icloud.com>
2024-04-28 08:34:41 +0530

    Added error handling.

--- app.rb
+++ app.rb
@@ -1,18 +1,44 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'mime/types'
-set :port, 3001
-set :bind, 'localhost'
 
-# IMAGES
-IMAGES_FOLDER = './images/'
+MEDIA_PATH = './media/'
+IMAGES_FOLDER = '/images/'
 
-get '/images/:image_name' do |image_name|
-     image_path = File.join(IMAGES_FOLDER, image_name)
-     if File.exist?(image_path)
-          content_type MIME::Types.type_for(image_path).first.to_s
-          send_file image_path
-     else
-          status 404
-          'File Not Found'
+class BookshelfStaticAssetServer < Sinatra::Base
+     
+     # ERROR HANDLING
+     error do
+          render_error 500, env['sinatra.error'].message
      end
+     
+     not_found do
+            render_error 404, 'Not found.'
+     end
+     
+     error 403 do
+            render_error 403, 'Forbidden.'
+     end
+     
+     def render_error code, message
+          halt code, erb(:error, locals: {status: code, message: message})
+     end
+     
+     error 405...510 do
+            erb(:error, locals: {status: -1, message: 'Unknown error.'})
+     end
+     
+     
+
+     # Routes
+     get '/images/*' do |image_subpath|
+          image_path = File.join(MEDIA_PATH, IMAGES_FOLDER, image_subpath)
+          if File.exist?(image_path)
+               content_type MIME::Types.type_for(image_path).first.to_s
+               send_file image_path
+          else
+               404
+          end
+     end
+       
+     run! if app_file == $0
 end
